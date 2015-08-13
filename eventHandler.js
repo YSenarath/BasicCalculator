19e01d99add24820539b765ee682e9c773d68a8c
nadundesilva <nadunrds@gmail.com>
2015-08-13 10:49:39 +0530

    added event handler

---
+++ eventHandler.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Nadun on 13-Aug-15.
+ */
+
+function equalButton() {
+
+}
+
+
+function clearButton() {
+    var elem = document.getElementById("input_area");
+    elem.value = "";
+}
+
+function dotButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + ".";
+}
+
+function zeroButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "0";
+}
+
+function oneButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "1";
+}
+
+function twoButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "2";
+}
+
+function threeButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "3";
+}
+
+function fourButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "4";
+}
+
+function fiveButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "5";
+}
+
+function sixButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "6";
+}
+
+function sevenButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "7";
+}
+
+function eightButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "8";
+}
+
+function nineButton() {
+    var button = document.getElementById('input_area');
+    button.value = button.value + "9";
+}
